b15acb5b61a274bda0c3ab5842e06480349ed2f9
jb-network <85036487+jb-network@users.noreply.github.com>
2022-07-11 21:51:50 -0500

    Add project files.

---
+++ The_Sieve/Program.cs
@@ -0,0 +1,47 @@
+﻿using The_Sieve;
+
+int UserChoice;
+do
+{
+    Console.WriteLine("Welcome to The Sieve number validator");
+    Console.WriteLine("You can pick from the following number filters for checking numbers");
+    Console.WriteLine("1 - Even Number");
+    Console.WriteLine("2 - Positive Number");
+    Console.WriteLine("3 - Multiple of Ten Uumber");
+    UserChoice = Convert.ToInt32(Console.ReadLine());
+    if (UserChoice != 1 && UserChoice != 2 && UserChoice != 3) Console.WriteLine("Please try again, your selection was not valid");
+} while (UserChoice != 1 && UserChoice != 2 && UserChoice != 3);
+
+
+Sieve NumberTester = UserChoice switch
+{
+    1 => new Sieve(EvenNumber),
+    2 => new Sieve(PositiveNumber),
+    3 => new Sieve(MultipleNumber)
+};
+
+while (true)
+{
+    Console.Write("Please provide a number to test: ");
+    int UserProvidednumber = Convert.ToInt32(Console.ReadLine());
+    
+    bool NumberTest = NumberTester.IsGood(UserProvidednumber);
+    if (NumberTest == true) Console.WriteLine($"The number {UserProvidednumber} that you provided passed our test.  This is a good number");
+    else Console.WriteLine($"The number {UserProvidednumber} that you provided failed out test. This is a bad number");
+}
+
+//Local Methods
+bool EvenNumber(int number)
+{
+    return number % 2 == 0;
+}
+
+bool PositiveNumber(int number)
+{
+    return number > 0;
+}
+
+bool MultipleNumber(int number)
+{
+    return number % 10 == 0;
+}
---
+++ The_Sieve/Sieve.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Sieve
+{
+    public class Sieve
+    {
+        private Func<int, bool> UserSelection;
+
+        public Sieve(Func<int, bool> UserChoice)
+        {
+            UserSelection = UserChoice;
+        }
+        public bool IsGood(int UserProvidednumber)
+        {
+            return UserSelection(UserProvidednumber);
+        }
+    }
+}
